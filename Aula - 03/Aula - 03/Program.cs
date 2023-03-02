6b3a2d9ee31b47a54491ef840eea5eac7ad9afb2
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-03-02 20:57:15 -0300

    Add files via upload

---
+++ Aula - 03/Aula - 03/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula___03
+{
+    internal class Program
+    {
+        //class Circle {
+
+        //    private int raio; //Constructor
+
+        //    public Circle()
+        //    {
+        //        raio = 2;
+        //    }
+
+        //    public double Area()
+        //    {
+        //        return (Math.PI * (raio * raio));
+        //    }
+
+
+        //}
+
+        //class Circle
+        //{
+
+        //    private int raio; //Constructor
+
+        //    public Circle()
+        //    {
+        //        raio = 0;
+        //    }
+
+        //    public Circle(int raioinicial) //Constructor overload
+        //    {
+        //        this.raio = raioinicial;
+        //    }
+
+        //    public double Area()
+        //    {
+        //        return (Math.PI * (raio * raio));
+        //    }
+
+
+        //}
+
+        class Circle
+        {
+
+            private int raio; //Constructor
+
+            public Circle()
+            {
+                raio = 0;
+            }
+
+            public Circle(int raioinicial) //Constructor overload
+            {
+                this.raio = raioinicial;
+            }
+
+            public double Area()
+            {
+                return (Math.PI * (raio * raio));
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(4); // com argumento ele chama o padrao, sem argumento ele chama o sobrecarregado
+            double areadocirculo;
+
+            areadocirculo = c.Area();
+            Console.WriteLine(areadocirculo);
+            Console.ReadKey();
+
+            Circle d = new Circle(4);
+
+            areadocirculo = d.Area();
+            Console.WriteLine(areadocirculo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula - 03/Aula 03 - 2/Aula 03 - 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_03___2
+{
+    internal class Program
+    {  
+        class Point 
+        { 
+            
+            private int x,y;
+            public Point() 
+            {
+                Console.WriteLine("Metodo Construtor chamado");
+            }
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                Console.WriteLine("Os valores das variaveis sao {0} e {1}" + this.x, this.y);
+            }  
+        }
+        static void Main(string[] args)
+        {
+            Point num = new Point();
+            Console.ReadKey();
+            Console.Clear();
+            num = new Point(4,5);
+            Console.ReadKey();
+        }
+    }
+}
